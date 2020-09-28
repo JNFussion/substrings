f056d434e7b65f85f2c90abcccaa36ba69fb021f
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-09-28 20:17:41 +0200

    formatting

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,15 @@
 def substring(str, dic)
-    aux = str.downcase
-    aux = aux.split(/[^A-Za-z]/)
-    aux.delete_if{|item| item.length <=1 && item != "a"}
-    
-    aux.reduce(Hash.new(0)) do |result, item| 
-        dic.each do |word|
-        
-            if item.include? word
-                result[word] += 1
-            end
-        end
-        
-        result
-    end
+  aux = str.downcase
+  aux = aux.split(/[^A-Za-z]/)
+  aux.delete_if { |item| item.length <= 1 && item != 'a' }
 
+  aux.each_with_object(Hash.new(0)) do |item, result|
+    dic.each do |word|
+      result[word] += 1 if item.include? word
+    end
+  end
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substring("below", dictionary)
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+puts substring('below', dictionary)
 puts substring("Howdy partner, sit down! How's it going?", dictionary)
